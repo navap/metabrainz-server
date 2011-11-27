39e94bcf340448c4ab46e07ed9e9749cbc3063b3
Pavan Chander <pchander@gmail.com>
2011-11-27 15:33:41 -0500

    Update donation templates

--- lib/MetaBrainz/Server/Controller/Donate.pm
+++ lib/MetaBrainz/Server/Controller/Donate.pm
@@ -2,9 +2,23 @@ package MetaBrainz::Server::Controller::Donate;
 BEGIN { use Moose; extends 'MusicBrainz::Server::Controller'; }
 use namespace::autoclean;
 
-sub paypal : Local {}
+sub paypal : Local
+{
+    my ($self, $c) = @_;
+
+    $c->stash(
+        amount => $c->req->params->{amount},
+        recur => $c->req->params->{recur},
+    );
+}
+
 sub complete : Local { }
 sub cancelled : Local { }
+
+sub index : Path Args(0)
+{
+    my ($self, $c) = @_;
+}
 
 sub add : Path('/admin/add-donation') {
     my ($self, $c) = @_;
